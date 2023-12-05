4d28c43f90d95641933ae14b1b701abdf32d3dd2
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-12-05 17:11:12 +0100

    genera escenarios superpuestos

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
 
-    public 
+    public GameObject[] habPetita;
+    public GameObject[] habMitjana;
+    public GameObject[] habGran;
 
-    // Start is called before the first frame update
+    public int alturaMapa = 10;
+    public int ampladaMapa = 10;
+
+    public int anterior = 0;
+
     void Start()
     {
-        
+        GenerateMap();
     }
 
-    // Update is called once per frame
+    void GenerateMap(){
+        for (int x = 0; x < ampladaMapa; x++){
+            for (int y = 0; y < alturaMapa; y++){
+                Vector3 spawnPosition = new Vector3(x * (ampladaMapa + 5f) + Random.Range(-5f, 5f), y * (alturaMapa+ 5f) + Random.Range(-5f, 5f), 0);
+                int nRandom = Random.Range(1,4);
+                if (anterior == 3){ //habitacio anterior es gran
+                    nRandom = Random.Range(1,3);
+                }
+                else if(anterior == 1){ //habitacio anterior es petita
+                    nRandom = Random.Range(2,4);
+                }
+
+                if (nRandom ==1){ //instanciar habitacio petita
+                    GameObject habitacioRandom = habPetita[Random.Range(0, habPetita.Length)];
+                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+                }
+                else if(nRandom == 2){ //instanciar habitacio mitjana
+                    GameObject habitacioRandom = habMitjana[Random.Range(0, habMitjana.Length)];
+                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+                }
+                else{ //instanciar habitacio gran
+                    GameObject habitacioRandom = habGran[Random.Range(0, habGran.Length)];
+                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+                }
+            
+             }
+
+        }
+    }
+
     void Update()
     {
         
