5ec271a5010cb04618d8ecb53e3514a6478158ef
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-12-05 21:16:30 +0100

    genera habitaciones lo que no del todo bien pero almenos no peta asi que de una tetes venga disfrutad

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -12,17 +12,30 @@ public class MapGenerator : MonoBehaviour
     public int alturaMapa = 10;
     public int ampladaMapa = 10;
 
+    public float petitaAmplada = 13f;
+    public float petitaAltura = 13f;
+    public float mitjanaAmplada = 19f;
+    public float mitjanaAltura = 19f;
+    public float granAmplada = 25f;
+    public float granAltura = 27f;
+
+    public float separacioHabitacions = 5f;
     public int anterior = 0;
+    private bool[,] quadriculaPos; 
 
     void Start()
     {
+        quadriculaPos = new bool[ampladaMapa,alturaMapa];
         GenerateMap();
     }
 
     void GenerateMap(){
+
+        List<Vector3> posUsades = new List<Vector3>();
         for (int x = 0; x < ampladaMapa; x++){
             for (int y = 0; y < alturaMapa; y++){
-                Vector3 spawnPosition = new Vector3(x * (ampladaMapa + 5f) + Random.Range(-5f, 5f), y * (alturaMapa+ 5f) + Random.Range(-5f, 5f), 0);
+                Vector3 spawnPosition = new Vector3(x * (ampladaMapa + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions), y * (petitaAltura + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions),0);
+                
                 int nRandom = Random.Range(1,4);
                 if (anterior == 3){ //habitacio anterior es gran
                     nRandom = Random.Range(1,3);
@@ -30,6 +43,15 @@ public class MapGenerator : MonoBehaviour
                 else if(anterior == 1){ //habitacio anterior es petita
                     nRandom = Random.Range(2,4);
                 }
+    
+                while(PosicioOcupada(spawnPosition, posUsades, nRandom) || QuadriculaOcupada(x,y)){ //comporbar que spawnPosition es buida
+                    spawnPosition = new Vector3(x * (ampladaMapa + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions), y * (alturaMapa+ separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions),0);
+                    x = Mathf.FloorToInt(spawnPosition.x / 1f);
+                    y = Mathf.FloorToInt(spawnPosition.y / 1f);
+                }
+
+                posUsades.Add(spawnPosition);
+                MarcarQuadricula(x,y);
 
                 if (nRandom ==1){ //instanciar habitacio petita
                     GameObject habitacioRandom = habPetita[Random.Range(0, habPetita.Length)];
@@ -47,6 +69,37 @@ public class MapGenerator : MonoBehaviour
              }
 
         }
+    }
+
+    bool QuadriculaOcupada(int x, int y){
+        return quadriculaPos[x,y];
+    }
+
+    void MarcarQuadricula(int x, int y){
+        quadriculaPos[x,y] = true;
+    }
+
+    bool PosicioOcupada(Vector3 pos, List<Vector3> posUsades, int tipus){
+        float amplada;
+        float altura;
+        if (tipus == 1){
+            amplada = petitaAmplada;
+            altura = petitaAltura;
+        }
+        else if (tipus == 2){
+            amplada = mitjanaAmplada;
+            altura = mitjanaAltura;
+        }
+        else{
+            amplada = granAmplada;
+            altura = granAltura;
+        }
+        foreach (Vector3 usada in posUsades){
+            if (Vector3.Distance(pos, usada) < amplada + separacioHabitacions){
+                return true;
+            }
+        }
+        return false;
     }
 
     void Update()
