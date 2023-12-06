3a11917513c2f8ae26c360688d3a0cbf65a1f3f3
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-12-06 12:23:24 +0100

    no se bro voy cambiando cosas y falla pero diferente pero ale

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -9,8 +9,9 @@ public class MapGenerator : MonoBehaviour
     public GameObject[] habMitjana;
     public GameObject[] habGran;
 
-    public int alturaMapa = 10;
-    public int ampladaMapa = 10;
+    public int habitacionsMapa = 5;
+    public float alturaMapa = 100f;
+    public float ampladaMapa = 100f;
 
     public float petitaAmplada = 13f;
     public float petitaAltura = 13f;
@@ -19,89 +20,86 @@ public class MapGenerator : MonoBehaviour
     public float granAmplada = 25f;
     public float granAltura = 27f;
 
-    public float separacioHabitacions = 5f;
-    public int anterior = 0;
-    private bool[,] quadriculaPos; 
+    public float separacioHabitacions = 1f;
+    public int anterior = 0; 
 
     void Start()
     {
-        quadriculaPos = new bool[ampladaMapa,alturaMapa];
         GenerateMap();
     }
 
     void GenerateMap(){
 
         List<Vector3> posUsades = new List<Vector3>();
-        for (int i = 0; i < ampladaMapa; i++){
-            for (int j = 0; j < alturaMapa; j++){
-                int x = i;
-                int y = j;
+        for (int i = 0; i < habitacionsMapa; i++){
                 
-                int nRandom = Random.Range(1,4);
-                if (anterior == 3){ //habitacio anterior es gran
-                    nRandom = Random.Range(1,3);
-                }
-                else if(anterior == 1){ //habitacio anterior es petita
-                    nRandom = Random.Range(2,4);
-                }
+            int nRandom = Random.Range(1,4);
+            if (anterior == 3){ //habitacio anterior es gran
+                nRandom = Random.Range(1,3);
+            }
+            else if(anterior == 1){ //habitacio anterior es petita
+                nRandom = Random.Range(2,4);
+            }
 
-                float ampladaHabitacio;
-                float alturaHabitacio;
+            float ampladaHabitacio;
+            float alturaHabitacio;
 
-                if (nRandom == 1){
-                    ampladaHabitacio = petitaAmplada;
-                    alturaHabitacio = petitaAltura;
-                }
-                else if (nRandom == 2){
-                    ampladaHabitacio = mitjanaAmplada;
-                    alturaHabitacio = mitjanaAltura;
-                }
-                else{
-                    ampladaHabitacio = granAmplada;
-                    alturaHabitacio = granAltura;
-                }
+            if (nRandom == 1){
+                ampladaHabitacio = petitaAmplada;
+                alturaHabitacio = petitaAltura;
+            }
+            else if (nRandom == 2){
+                ampladaHabitacio = mitjanaAmplada;
+                alturaHabitacio = mitjanaAltura;
+            }
+            else{
+                ampladaHabitacio = granAmplada;
+                alturaHabitacio = granAltura;
+            }
 
-                Vector3 spawnPosition = new Vector3(x * (ampladaHabitacio + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions), y * (alturaHabitacio + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions),0);
-    
-                while(PosicioOcupada(spawnPosition, posUsades, nRandom) || QuadriculaOcupada(x,y)){ //comporbar que spawnPosition es buida
-                    spawnPosition = new Vector3(x * (ampladaHabitacio + separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions), y * (alturaHabitacio+ separacioHabitacions) + Random.Range(-separacioHabitacions, separacioHabitacions),0);
-                    x = Mathf.FloorToInt(spawnPosition.x / 1f);
-                    y = Mathf.FloorToInt(spawnPosition.y / 1f);
-                }
+            Debug.Log("se escoge tipo habitacion");
 
-                posUsades.Add(spawnPosition);
-                MarcarQuadricula(x,y);
-                anterior = nRandom;
+            float x = Random.Range(ampladaHabitacio, ampladaMapa - ampladaHabitacio);
+            float y = Random.Range(alturaHabitacio, alturaMapa - alturaHabitacio);
 
-                if (nRandom ==1){ //instanciar habitacio petita
-                    GameObject habitacioRandom = habPetita[Random.Range(0, habPetita.Length)];
-                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
-                }
-                else if(nRandom == 2){ //instanciar habitacio mitjana
-                    GameObject habitacioRandom = habMitjana[Random.Range(0, habMitjana.Length)];
-                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
-                }
-                else{ //instanciar habitacio gran
-                    GameObject habitacioRandom = habGran[Random.Range(0, habGran.Length)];
-                    GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
-                }
-            
-             }
+            Vector3 spawnPosition = new Vector3(x,y,0);
+            Debug.Log("punto de spawn");
+            bool ocupada = PosicioOcupada(spawnPosition, posUsades, nRandom);
+            int intentos = 0;
 
+            while(ocupada && intentos < 100){ //comporbar que spawnPosition es buida
+                x = Random.Range(ampladaHabitacio, ampladaMapa - ampladaHabitacio);
+                y = Random.Range(alturaHabitacio, alturaMapa - alturaHabitacio);
+                spawnPosition = new Vector3(x,y,0);
+
+                ocupada = PosicioOcupada(spawnPosition, posUsades, nRandom);
+                intentos++;
+            }
+
+            Debug.Log("punto de spawn correcto");
+            posUsades.Add(spawnPosition);
+            anterior = nRandom;
+
+            if (nRandom ==1){ //instanciar habitacio petita
+                GameObject habitacioRandom = habPetita[Random.Range(0, habPetita.Length)];
+                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+                }
+            else if(nRandom == 2){ //instanciar habitacio mitjana
+                GameObject habitacioRandom = habMitjana[Random.Range(0, habMitjana.Length)];
+                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);                }
+            else{ //instanciar habitacio gran
+                GameObject habitacioRandom = habGran[Random.Range(0, habGran.Length)];
+                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+            }
+            Debug.Log("instanciada");
         }
-    }
-
-    bool QuadriculaOcupada(int x, int y){
-        return quadriculaPos[x,y];
-    }
-
-    void MarcarQuadricula(int x, int y){
-        quadriculaPos[x,y] = true;
+        
     }
 
     bool PosicioOcupada(Vector3 pos, List<Vector3> posUsades, int tipus){
         float amplada;
-        float altura;
+        float altura; 
+        Debug.Log("entra en posicion Ocupada");
         if (tipus == 1){
             amplada = petitaAmplada;
             altura = petitaAltura;
@@ -116,9 +114,11 @@ public class MapGenerator : MonoBehaviour
         }
         foreach (Vector3 usada in posUsades){
             if (Vector3.Distance(pos, usada) < amplada + separacioHabitacions){
+                Debug.Log("posicion ocupada");
                 return true;
             }
         }
+        Debug.Log("posicion libre");
         return false;
     }
 
