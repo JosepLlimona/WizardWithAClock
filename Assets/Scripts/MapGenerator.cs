3c8c5b3d13f3ea02f1f062ebeb767988369b5628
u1979255 <125899452+u1979255@users.noreply.github.com>
2023-12-07 13:34:59 +0100

    No se como pero no toqueis ni lo mas minimo del codigo pk funciona

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,22 +6,26 @@ public class MapGenerator : MonoBehaviour
 {
 
     public GameObject[] habPetita;
+    private List<int> petitaUsades = new List<int>();
     public GameObject[] habMitjana;
+    private List<int> mitjanaUsades = new List<int>();
     public GameObject[] habGran;
+    private List<int> granUsades = new List<int>();
 
-    public int habitacionsMapa = 5;
-    public float alturaMapa = 100f;
-    public float ampladaMapa = 100f;
+    public int habitacionsMapa;
+    public float alturaMapa;
+    public float ampladaMapa;
 
-    public float petitaAmplada = 13f;
-    public float petitaAltura = 13f;
-    public float mitjanaAmplada = 19f;
-    public float mitjanaAltura = 19f;
-    public float granAmplada = 25f;
-    public float granAltura = 27f;
+    private float petitaAmplada = 7f;
+    private float petitaAltura = 7f;
+    private float mitjanaAmplada = 8f;
+    private float mitjanaAltura = 8f;
+    private float granAmplada = 9f;
+    private float granAltura = 9f;
 
     public float separacioHabitacions = 1f;
-    public int anterior = 0; 
+    private int anterior = 0; 
+
 
     void Start()
     {
@@ -33,22 +37,58 @@ public class MapGenerator : MonoBehaviour
         List<Vector3> posUsades = new List<Vector3>();
         for (int i = 0; i < habitacionsMapa; i++){
                 
-            int nRandom = Random.Range(1,4);
-            if (anterior == 3){ //habitacio anterior es gran
-                nRandom = Random.Range(1,3);
-            }
-            else if(anterior == 1){ //habitacio anterior es petita
-                nRandom = Random.Range(2,4);
-            }
-
+            int tipus; 
             float ampladaHabitacio;
             float alturaHabitacio;
 
-            if (nRandom == 1){
+            ObtenirTipusHabitacio(out tipus, out ampladaHabitacio, out alturaHabitacio);
+
+            float x = Random.Range(-ampladaMapa + ampladaHabitacio, ampladaMapa - ampladaHabitacio);
+            float y = Random.Range(-alturaMapa + alturaHabitacio, alturaMapa - alturaHabitacio);
+
+            Vector3 spawnPosition = new Vector3(x,y,0);
+            
+            bool ocupada = PosicioOcupada(spawnPosition, posUsades, tipus);
+            int intentos = 0;
+
+            while(ocupada && intentos < 100){ //comporbar que spawnPosition es buida
+                x = Random.Range(-ampladaMapa + ampladaHabitacio, ampladaMapa - ampladaHabitacio);
+                y = Random.Range(-alturaMapa + alturaHabitacio, alturaMapa - alturaHabitacio);
+                spawnPosition = new Vector3(x,y,0);
+
+                ocupada = PosicioOcupada(spawnPosition, posUsades, tipus);
+                intentos++;
+            }
+
+            posUsades.Add(spawnPosition);
+            anterior = tipus;
+
+            GameObject habitacioRandom = ObtenirHabitacioRandom(tipus);
+            GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
+        }
+        
+    }
+
+    void ObtenirTipusHabitacio(out int tipus, out float ampladaHabitacio, out float alturaHabitacio){
+        tipus = Random.Range(1,4);
+            if (anterior == 3 || habGran.Length == granUsades.Count){ //habitacio anterior es gran o llista gran usada
+                tipus = Random.Range(1,3);
+            }
+            else if(anterior == 1 || habPetita.Length == petitaUsades.Count){ //habitacio anterior es petita o llista petita usada
+                tipus = Random.Range(2,4);
+            }
+            else if (habMitjana.Length == mitjanaUsades.Count){ //llista mitjana usada
+                tipus = Random.Range(2,4);
+                if (tipus == 2){
+                    tipus = 1;
+                }
+            }
+
+            if (tipus == 1){
                 ampladaHabitacio = petitaAmplada;
                 alturaHabitacio = petitaAltura;
             }
-            else if (nRandom == 2){
+            else if (tipus == 2){
                 ampladaHabitacio = mitjanaAmplada;
                 alturaHabitacio = mitjanaAltura;
             }
@@ -56,50 +96,49 @@ public class MapGenerator : MonoBehaviour
                 ampladaHabitacio = granAmplada;
                 alturaHabitacio = granAltura;
             }
+    }
 
-            Debug.Log("se escoge tipo habitacion");
+    GameObject ObtenirHabitacioRandom(int tipus){
+        GameObject habitacio;
+        if (tipus ==1){ //agafar habitacio petita
 
-            float x = Random.Range(ampladaHabitacio, ampladaMapa - ampladaHabitacio);
-            float y = Random.Range(alturaHabitacio, alturaMapa - alturaHabitacio);
-
-            Vector3 spawnPosition = new Vector3(x,y,0);
-            Debug.Log("punto de spawn");
-            bool ocupada = PosicioOcupada(spawnPosition, posUsades, nRandom);
-            int intentos = 0;
-
-            while(ocupada && intentos < 100){ //comporbar que spawnPosition es buida
-                x = Random.Range(ampladaHabitacio, ampladaMapa - ampladaHabitacio);
-                y = Random.Range(alturaHabitacio, alturaMapa - alturaHabitacio);
-                spawnPosition = new Vector3(x,y,0);
-
-                ocupada = PosicioOcupada(spawnPosition, posUsades, nRandom);
-                intentos++;
+            int index = Random.Range(0, habPetita.Length);
+            while (petitaUsades.Contains(index)){
+                index = Random.Range(0, habPetita.Length);
             }
 
-            Debug.Log("punto de spawn correcto");
-            posUsades.Add(spawnPosition);
-            anterior = nRandom;
-
-            if (nRandom ==1){ //instanciar habitacio petita
-                GameObject habitacioRandom = habPetita[Random.Range(0, habPetita.Length)];
-                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
-                }
-            else if(nRandom == 2){ //instanciar habitacio mitjana
-                GameObject habitacioRandom = habMitjana[Random.Range(0, habMitjana.Length)];
-                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);                }
-            else{ //instanciar habitacio gran
-                GameObject habitacioRandom = habGran[Random.Range(0, habGran.Length)];
-                GameObject instaciarHabitacio = Instantiate(habitacioRandom, spawnPosition, Quaternion.identity);
-            }
-            Debug.Log("instanciada");
+            petitaUsades.Add(index);
+            habitacio = habPetita[index];
         }
-        
+
+        else if(tipus == 2){ //agafar habitacio mitjana
+
+            int index = Random.Range(0, habMitjana.Length);
+            while (mitjanaUsades.Contains(index)){
+                index = Random.Range(0, habMitjana.Length);
+            }
+
+            mitjanaUsades.Add(index);
+            habitacio = habMitjana[index];
+        }
+
+        else{ //agafar habitacio gran
+
+            int index = Random.Range(0, habGran.Length);
+            while (granUsades.Contains(index)){
+                index = Random.Range(0, habGran.Length);
+            }
+
+            granUsades.Add(index);
+            habitacio = habGran[index];
+        }
+        return habitacio;
     }
 
     bool PosicioOcupada(Vector3 pos, List<Vector3> posUsades, int tipus){
         float amplada;
         float altura; 
-        Debug.Log("entra en posicion Ocupada");
+        
         if (tipus == 1){
             amplada = petitaAmplada;
             altura = petitaAltura;
@@ -114,16 +153,9 @@ public class MapGenerator : MonoBehaviour
         }
         foreach (Vector3 usada in posUsades){
             if (Vector3.Distance(pos, usada) < amplada + separacioHabitacions){
-                Debug.Log("posicion ocupada");
                 return true;
             }
         }
-        Debug.Log("posicion libre");
         return false;
-    }
-
-    void Update()
-    {
-        
     }
 }
