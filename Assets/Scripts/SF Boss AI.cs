75f5b524f9d012720ee66eb5a997dfd7b2943516
CarlosMelgosa <u1979254@campus.udg.edu>
2023-12-17 21:39:10 +0100

    afegides animacions al animator del boss

--- Assets/Scripts/SF Boss AI.cs
+++ Assets/Scripts/SF Boss AI.cs
@@ -1,5 +1,10 @@
 using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class SFBossAI : MonoBehaviour
 {
@@ -15,11 +20,14 @@ public class SFBossAI : MonoBehaviour
     private int num1 = 0;
     private int num2 = 0;
     private int AtackOption;
+    private Vector2 objective;
+    [SerializeField]
+    private Animator BossAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        AtackOption= Random.Range(0, 20);
+        AtackOption = Random.Range(0, 20);
         rbody = GetComponent<Rigidbody2D>();
         InvokeRepeating("moveboss", 0f, timemove);
     }
@@ -27,9 +35,13 @@ public class SFBossAI : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        if (atack)
+        {
+            print("atacking i cant move");
+        }
         if (canmove && !atack)
         {
-            Vector2 direction = new Vector2(player.transform.position.x + num1 - transform.position.x, player.transform.position.y + num2 - transform.position.y);
+            Vector2 direction = objective;
             rbody.velocity = direction.normalized * speed;
         }
     }
@@ -38,21 +50,28 @@ public class SFBossAI : MonoBehaviour
     {
         num1 = Random.Range(-5, 6);
         num2 = Random.Range(-5, 6);
+        if (num1 < 2 && num1 > -2)
+        {
+            num1 = 2;
+        }
+        if (num2 < 2 && num1 > -2)
+        {
+            num2 = 2;
+        }
+        objective = new Vector2(player.transform.position.x + num1 - transform.position.x, player.transform.position.y + num2 - transform.position.y);
         canmove = !canmove;
         if (!canmove)
         {
             rbody.velocity = Vector3.zero;
             rbody.angularVelocity = 0;
-            
+
             if (AtackOption >= 0 && AtackOption < 2 && !atack)
             {
-                
                 portalPunch();
             }
             else if (AtackOption >= 2 && AtackOption < 4 && !atack)
             {
-                
-                TpPunch();
+                TpPunchCharge();
             }
             else if (!atack)
             {
@@ -64,25 +83,35 @@ public class SFBossAI : MonoBehaviour
 
     private void portalPunch()
     {
-        atack = true;
+        BossAnim.SetBool("PreparingPortalP", false);
         print("doing portal Punch");
-        atack = false;
         AtackOption = Random.Range(0, 11);
     }
-
+    private void TpPunchCharge()
+    {
+        BossAnim.SetBool("TpCharging",true);
+    }
     private void TpPunch()
     {
-        atack = true;
-        print("doing Tp Punch");
-        rbody.position = new Vector2(player.transform.position.x + 1 , player.transform.position.y );
-        atack = false;
+        BossAnim.SetBool("TpCharging", false);
+        BossAnim.SetBool("TpPunchReady", true);
+        rbody.position = new Vector2(player.transform.position.x + 1, player.transform.position.y);
+        Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+        rbody.velocity = direction.normalized * speed * 2;
         AtackOption = Random.Range(0, 11);
     }
     private void RayGun()
     {
-        atack = true;
         print("doing RayGun");
-        atack = false;
         AtackOption = Random.Range(0, 11);
+    }
+
+    private void startAtack()
+    {
+        atack = true;
+    }
+    private void finishAtack()
+    {
+        atack = false;
     }
 }
