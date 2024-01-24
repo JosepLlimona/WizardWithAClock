bef6d477b445fd0ecf5d1263de66f77345ab77a3
jrubi272 <jaumerubi7000@gmail.com>
2024-01-24 16:30:34 +0100

    Digital Enemic Fet :)

---
+++ Assets/Scripts/Numbers/Num3.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Num3 : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private bool alreadyShot = false;
+
+    public void setPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(DestroyAfterDelay(4f));
+    }
+
+    private void FixedUpdate()
+    {
+        if (!alreadyShot)
+        {
+            Vector2 direction = new Vector2(player.transform.position.x - rbody.transform.position.x, player.transform.position.y - rbody.transform.position.y);
+            rbody.velocity = direction.normalized * 1.5f;
+            alreadyShot = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Tocant desde bala");
+            player.GetComponent<PlayerController>().lostLife(15);
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Numbers/Num4.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Num4 : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private bool alreadyShot = false;
+
+
+
+    public void setPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        StartCoroutine(DestroyAfterDelay(5f));
+    }
+
+    private void FixedUpdate()
+    {
+        if (!alreadyShot)
+        {
+            Vector2 direction = new Vector2(player.transform.position.x - rbody.transform.position.x, player.transform.position.y - rbody.transform.position.y);
+            rbody.velocity = direction.normalized * 0.8f;
+            alreadyShot = true;
+        }
+        else
+        {
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Tocant desde bala");
+            player.GetComponent<PlayerController>().lostLife(20);
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+    }
+}
