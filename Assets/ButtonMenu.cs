52a3256b8cc68a6e11ab58d40de415bf90a3082b
CompCube <jordibp.valls@gmail.com>
2024-01-27 22:13:40 +0100

    hh

---
+++ Assets/ButtonMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMenu : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        // Load the main scene (you can replace "MainScene" with the actual scene name)
+        SceneManager.LoadScene("Demo");
+    }
+    public void OnEndButtonClick()
+    {
+        // Load the main scene (you can replace "MainScene" with the actual scene name)
+        //tanca el joc
+        Application.Quit();
+    }
+}
